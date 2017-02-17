2e4641ac219da4f647c13fd807d5bd682874c635
Nabu1014 <onli.xbox@hotmail.com>
2017-02-17 15:51:46 -0600

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+
+int main(){
+    
+    struct hoja{
+        int edad;
+        char sexo;
+        float promedio;
+        
+            
+    };
+
+    struct Hoja inscripcion;
+    struct Hoja *finalizar
+    struct hoja divorcios[200];
+    
+    inscripcion.edad = 22;
+    finalñizar->edad = 22;
+    divoprcios[30].edad = 21;
+    
+return(0);
+}
