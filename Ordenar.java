ac98ed1461792d81cedfc0438705b6f43bff4bf0
Nabu1014 <onli.xbox@hotmail.com>
2017-03-24 10:26:12 -0600

    Create Ordenar.java
    método de burbuja en java

---
+++ Ordenar.java
@@ -0,0 +1,26 @@
+public class Ordenar{
+
+	void burbuja(int arreglo[]){
+
+		int i, j, t;
+
+		for(i=0;i<arreglo.length;i++){
+
+			for(j=0;j<arreglo.length;j++){
+				if(arreglo[i]<arreglo[j])
+				temp = arreglo[i];
+				arreglo[i]=arreglo[j];
+				arreglo[j]=temp;
+			}
+		}
+	}
+
+
+
+	for(i=0<arreglo.length;i++){
+
+		System.out.println(arreglo[i])
+	}
+
+
+}
